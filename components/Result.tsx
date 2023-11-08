f7a4689199d01d0720ed8e36d97e613bb78dc040
“maidaasl” <maida.saniyah@ui.ac.id>
2023-11-08 16:46:58 +0700

    feat: improve result ui

--- components/Result.tsx
+++ components/Result.tsx
@@ -1,4 +1,3 @@
-// components/Result.tsx
 import React from "react";
 
 interface ResultProps {
@@ -8,9 +7,14 @@ interface ResultProps {
 
 export const Result: React.FC<ResultProps> = ({ score, onNewPractice }) => {
   return (
-    <div>
-      <h2>Your Score: {score}</h2>
-      <button onClick={onNewPractice}>Start New Practice</button>
+    <div className="bg-white p-6 text-center">
+      <p className="text-3xl font-semibold text-purple-600 mb-4">Your Score: {score}</p>
+      <button
+        className="bg-purple-600 text-white px-4 py-2 rounded-full hover:bg-purple-700 transition-all"
+        onClick={onNewPractice}
+      >
+        Start New Practice
+      </button>
     </div>
   );
 };
